3ac2143ee1f567a9b64d7d19ccc17bf20cb296bf
wahebwahbe100-byte <wahebwahbe100@gmail.com>
2026-03-24 19:31:59 +0100

    Delete assets directory

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-
-const toggle = document.querySelector('.mobile-toggle');
-const header = document.querySelector('header');
-if(toggle && header){
-  toggle.addEventListener('click', () => header.classList.toggle('mobile-open'));
-}
