5de382a02cff84723ffda4dbb90e47b00baac19c
wahebwahbe100-byte <wahebwahbe100@gmail.com>
2026-03-25 02:05:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll('button[type="button"]').forEach(btn=>{btn.addEventListener("click",()=>{alert("Dies ist eine statische Demo. Das Formular kann später mit E-Mail, WhatsApp oder einem Backend verbunden werden.");});});});
