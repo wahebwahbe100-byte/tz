60f7a9f81b6d5829c5ebd0671158b15c0e69d86a
wahebwahbe100-byte <wahebwahbe100@gmail.com>
2026-03-25 02:13:40 +0100

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll('button[type="button"]').forEach(btn=>{btn.addEventListener("click",()=>{alert("Dies ist eine statische Demo. Das Formular kann später mit E-Mail, WhatsApp oder einem Backend verbunden werden.");});});});
